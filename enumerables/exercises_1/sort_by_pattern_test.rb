e69f34355c6af9a89ec483689a4451c9e09c48ce
James Cape <james.cape@gmail.com>
2019-03-16 23:12:00 -0600

    Pass all tests

--- enumerables/exercises_1/sort_by_pattern_test.rb
+++ enumerables/exercises_1/sort_by_pattern_test.rb
@@ -1,6 +1,7 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class SortByPatternTest < Minitest::Test
 
@@ -19,10 +20,11 @@ class SortByPatternTest < Minitest::Test
   end
 
   def test_sort_alphabetically_by_last_letter
+
     things = ["pill", "box", "glass", "water", "sponge"]
     transformed = []
     things.each do |thing|
-      # Your code goes here
+      transformed << [thing.reverse, thing]
     end
     transformed = transformed.sort
     sorted = []
@@ -33,36 +35,67 @@ class SortByPatternTest < Minitest::Test
   end
 
   def test_sort_by_distance
-    skip
+
     distances = ["1cm", "9cm", "30cm", "4cm", "2cm"]
     transformed = []
-    # Your code goes here
+    distances.each do |dist|
+      transformed << [dist.delete("cm").to_i, dist]
+    end
+
     transformed = transformed.sort
     sorted = []
-    transformed.each do |sort_key, distance|
-      sorted << distance
+    transformed.each do |sort_key, dist|
+      sorted << dist
     end
     assert_equal ["1cm", "2cm", "4cm", "9cm", "30cm"], sorted
   end
 
   def test_sort_by_length
-    skip
+
     words = ["heteromorph", "ancyloceratina", "bioengineering", "mathematical", "bug"]
-    # Your code goes here
+    transformed = []
+    words.each do |word|
+        transformed << [word.length, word]
+    end
+
+    transformed = transformed.sort
+    sorted = []
+    transformed.each do |sort_key, word|
+      sorted << word
+    end
     assert_equal ["bug", "heteromorph", "mathematical", "ancyloceratina", "bioengineering"], sorted
   end
 
   def test_sort_by_proximity_to_ten
-    skip
+
     prices = [3.02, 9.91, 17.9, 10.01, 11.0]
-    # Your code goes here
+    transformed = []
+    prices.each do |price|
+      transformed << [(10.0 - price).abs, price]
+    end
+
+    transformed = transformed.sort
+    sorted = []
+    transformed.each do |sort_key, price|
+      sorted << price
+    end
     assert_equal [10.01, 9.91, 11.0, 3.02, 17.9], sorted
   end
 
   def test_sort_by_number_of_cents
-    skip
+
     prices = [3.02, 9.91, 7.9, 10.01, 11.0]
-    # Your code goes here
+    transformed = []
+    prices.each do |price|
+      transformed << [(price * 100) % 100, price]
+    end
+
+    transformed = transformed.sort
+    sorted = []
+    transformed.each do |sort_key, price|
+      sorted << price
+    end
+    
     assert_equal [11.0, 10.01, 3.02, 7.9, 9.91], sorted
   end
 
